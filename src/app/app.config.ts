32154060ada2482e23c9439578e42a1377464ab9
Patrick Ayala <patrick.ayala@csiweb.com>
2026-02-03 14:01:32 -0600

    update app to use csi theme

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,14 +3,14 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { providePrimeNG } from 'primeng/config';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import AcmeTheme from './acme-theme';
+import CSITheme from './csi-theme';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideAnimationsAsync(),
     providePrimeNG({
       theme: {
-        preset: AcmeTheme,
+        preset: CSITheme,
         options: { darkModeSelector: '.p-dark' },
       },
     }),
